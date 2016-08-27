6a7e18b6245e17b3285c2ad722b8a61593e74596
hant0508 <hant0508@gmail.com>
2016-08-27 17:22:34 +0400

    Add license, little code refactoring

--- src/util.hpp
+++ src/util.hpp
@@ -1,16 +1,40 @@
+/*
+	Copyright (c) 2016 Dmitry Ivanov <hant0508@gmail.com>
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, this
+	   list of conditions and the following disclaimer.
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
-#define S_TRUE			(std::string ("true"))
-#define S_FALSE			(std::string ("false"))
-#define S_EXTENSION		(std::string (".desktop"))
-#define S_LOCAL_FILE	(std::string ("file://"))
-#define S_SLASH			(std::string ("/"))
-#define S_SPACE			(std::string (" "))
-#define S_EMPTY			(std::string (""))
-#define S_CONFIG_PATH	(std::string (".config/autostart/"))
-#define S_SETTINGS_PATH	(std::string ("/usr/share/startup-settings/gui.glade"))
-
-#define ERR_NF			(std::string ("File not found or permission denied: "))
+const char S_TRUE[] =           "true";
+const char S_FALSE[] =          "false";
+const char S_EXTENSION[] =      ".desktop";
+const char S_LOCAL_FILE[] =     "file://";
+const char S_SLASH[] =          "/";
+const char S_SPACE[] =          " ";
+const char S_EMPTY[] =          "";
+const char S_CONFIG_PATH[] =    ".config/autostart/";
+const char S_SETTINGS_PATH[] =  "/usr/share/startup-settings/gui.glade";
+const char ERR_NF[] =           "File not found or permission denied: ";
 
 #endif // UTIL_HPP
