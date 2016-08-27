7664fa1379f6cff517fc23bdca71b3c90b5261c9
hant0508 <hant0508@gmail.com>
2016-08-27 17:18:35 +0400

    Add license, little code refactoring

--- src/item.cpp
+++ src/item.cpp
@@ -1,7 +1,32 @@
+/*
+	Copyright (c) 2016 Dmitry Ivanov <hant0508@gmail.com>
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, this
+	   list of conditions and the following disclaimer.
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "item.hpp"
 #include <cstdio>
 
-Item::Item (std::string cmd, std::string name, std::string comment, bool term)
+Item::Item (const std::string &cmd, const std::string &name, const std::string &comment, bool term)
 {
 	set_cmd (cmd);
 	set_name (name);
@@ -9,19 +34,19 @@ Item::Item (std::string cmd, std::string name, std::string comment, bool term)
 	set_term (term);
 }
 
-void Item::set_cmd (std::string cmd)
+void Item::set_cmd (const std::string &cmd)
 {
 	cmd_ = cmd;
 }
 
-void Item::set_name (std::string name)
+void Item::set_name (const std::string &name)
 {
 	name_ = name;
 }
 
-void Item::set_comment (std::string comment)
+void Item::set_comment (const std::string &comment)
 {
-	comment_ = comment; // TODO
+	comment_ = comment;
 }
 
 void Item::set_term (bool term)
